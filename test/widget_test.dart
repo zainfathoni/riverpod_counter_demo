32561779aeca1bc5b0b78f067eb93b3336eefe43
Zain Fathoni <me@zainf.dev>
2021-06-22 09:14:48 +0800

    Fix the test by providing ProviderScope when building MyApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:my_counter_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(ProviderScope(child: MyApp()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
